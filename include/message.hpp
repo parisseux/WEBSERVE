913328b4eada2b8d993b3be3daff644af16bb163
Mawuli-Kplim Asase <masase@c1r17s3.42lausanne.ch>
2025-12-23 17:35:31 +0100

    adding parsing function

--- include/message.hpp
+++ include/message.hpp
@@ -8,25 +8,23 @@
 // Only PATCH, POST, and PUT requests have a body
 //The presence of a message body in a request is signaled by a Content-Length or Transfer-Encoding header field
 
-struct header
-{
-    std::map<std::string, std::string> header;
-};
-
 class request
 {
     private:
-        std::string method; // GET, POST, PUT....
-        std::string request_target; // element sur lequel va etre applique la methode
-        std::string protocol; // HTTP/1.1. en general
-        header header;
-        std::string body; // pas sur de devoir le stocker comme cela
+        std::string _method; // GET, POST, PUT....
+        std::string _request_target; // element sur lequel va etre applique la methode
+        std::string _protocol; // HTTP/1.1. en general
+        std::map<std::string, std::string> _header;
+        std::string _body; // pas sur de devoir le stocker comme cela
     public:
         request(std::string);
         request(const request &obj);
         ~request();
         request& operator=(const request &obj);
         void parseRequest(char *request);
+        void parse_request_first_line(std::ifstream &stream);
+        void parse_header(std::ifstream &stream);
+        void display_request();
 };
 
 struct response
@@ -34,7 +32,7 @@ struct response
     std::string protocol; // HTTP/1.1. en general
     std::string status_code; // code de sortie par rapport a la request (200 OK, 404 not found)
     std::string reason_phrase; // optionel pour decrire le code de sortie.
-    header header;
+    std::map<std::string, std::string> _header;
     std::string body; // pas sur de devoir le stocker comme cela    
 };
 
--- src/request_parser.cpp
+++ src/request_parser.cpp
@@ -1,5 +1,10 @@
 #include "../include/message.hpp"
 
+request::request(std::string)
+{
+    
+}
+
 void    request::parseRequest(char *request)
 {
     std::ifstream request_stream(request);
@@ -9,6 +14,47 @@ void    request::parseRequest(char *request)
         throw std::runtime_error("couldn't open the request");
     while (std::getline(request_stream, line))
     {
-        
+        parse_request_first_line(request_stream);
+        parse_header(request_stream);
     }   
+}
+
+void    request::parse_request_first_line(std::ifstream &stream)
+{
+    std::string word;
+
+    stream >> word;
+    _method = word;
+    stream >> word;
+    _request_target = word;
+    stream >> word;
+    _protocol = word;
+}
+
+void request::parse_header(std::ifstream &stream)
+{
+    std::string key;
+    std::string line;
+    while (std::getline(stream, key, ':'))
+    {
+
+        while(std::getline(stream, line, '\n'))
+        {
+            _header[key] = line;
+        }
+    }
+}
+
+void request::display_request()
+{
+    std::map<std::string, std::string>::iterator it = _header.begin();
+    std::cout << _method << " "
+            <<  _request_target << " "
+            << _protocol << std::endl;
+    while (it != _header.end())
+    {
+        std::cout << it->first << " : " << it->second << std::endl;
+        ++it;
+    }
+    // body
 }
